81ccb974ef8e3441bcc926f402f46c95012bb1a3
强 梁 <673615750@qq.com>
2021-01-16 15:47:06 +0800

    编写同步、异步测试用例。

---
+++ StudyThread/Main.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace StudyThread
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Sync = new System.Windows.Forms.Button();
+            this.btn_Async = new System.Windows.Forms.Button();
+            this.rtb_Sync = new System.Windows.Forms.RichTextBox();
+            this.rtb_Async = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btn_Sync
+            // 
+            this.btn_Sync.Location = new System.Drawing.Point(124, 560);
+            this.btn_Sync.Name = "btn_Sync";
+            this.btn_Sync.Size = new System.Drawing.Size(75, 23);
+            this.btn_Sync.TabIndex = 0;
+            this.btn_Sync.Text = "同步方法";
+            this.btn_Sync.UseVisualStyleBackColor = true;
+            this.btn_Sync.Click += new System.EventHandler(this.btn_Sync_Click);
+            // 
+            // btn_Async
+            // 
+            this.btn_Async.Location = new System.Drawing.Point(826, 560);
+            this.btn_Async.Name = "btn_Async";
+            this.btn_Async.Size = new System.Drawing.Size(75, 23);
+            this.btn_Async.TabIndex = 1;
+            this.btn_Async.Text = "异步方法";
+            this.btn_Async.UseVisualStyleBackColor = true;
+            this.btn_Async.Click += new System.EventHandler(this.btn_Async_Click);
+            // 
+            // rtb_Sync
+            // 
+            this.rtb_Sync.Location = new System.Drawing.Point(-1, 0);
+            this.rtb_Sync.Name = "rtb_Sync";
+            this.rtb_Sync.Size = new System.Drawing.Size(586, 545);
+            this.rtb_Sync.TabIndex = 2;
+            this.rtb_Sync.Text = "";
+            // 
+            // rtb_Async
+            // 
+            this.rtb_Async.Location = new System.Drawing.Point(591, 0);
+            this.rtb_Async.Name = "rtb_Async";
+            this.rtb_Async.Size = new System.Drawing.Size(525, 545);
+            this.rtb_Async.TabIndex = 3;
+            this.rtb_Async.Text = "";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1119, 633);
+            this.Controls.Add(this.rtb_Async);
+            this.Controls.Add(this.rtb_Sync);
+            this.Controls.Add(this.btn_Async);
+            this.Controls.Add(this.btn_Sync);
+            this.Name = "Main";
+            this.Text = "多线程研究";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Sync;
+        private System.Windows.Forms.Button btn_Async;
+        private System.Windows.Forms.RichTextBox rtb_Sync;
+        private System.Windows.Forms.RichTextBox rtb_Async;
+    }
+}
+
---
+++ StudyThread/Main.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudyThread
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            //为了显示到rtb控件，防止捕获异步线程调用错误。
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        /// <summary>
+        /// 同步测试按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Sync_Click(object sender, EventArgs e)
+        {
+            rtb_Sync.Clear();
+            rtb_Sync.AppendText(string.Format("当前同步方法开始 ID：{0} \n", Thread.CurrentThread.ManagedThreadId));
+            var j = 0;
+            var k = 1;
+            var m = j + k;
+
+            for (int i = 0; i < 5; i++)
+            {
+                var name = string.Format("{0}_{1}", "btn_Sync_Click", i);
+                DoSomethingLong(name, rtb_Sync);
+            }
+            rtb_Sync.AppendText(string.Format("当前同步方法结束 ID：{0}\n ", Thread.CurrentThread.ManagedThreadId));
+        }
+
+        /// <summary>
+        /// 异步测试按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Async_Click(object sender, EventArgs e)
+        {
+            rtb_Async.Clear();
+            rtb_Async.AppendText(string.Format("当前异步方法开始 ID：{0} ", Thread.CurrentThread.ManagedThreadId));
+            Action<string, RichTextBox> action = DoSomethingLong;
+            action.Invoke("btn_Async_Click_1", rtb_Async);
+            action("btn_Async_Click_2", rtb_Async);
+            action.BeginInvoke("btn_Async_Click_3", rtb_Async, null, null);//异步
+            rtb_Async.AppendText(string.Format("当前异步方法结束 ID：{0} ", Thread.CurrentThread.ManagedThreadId));
+        }
+
+        /// <summary>
+        /// 测试方法
+        /// </summary>
+        /// <param name="name"></param>
+        private void DoSomethingLong(string name, RichTextBox rtb)
+        {
+            rtb.AppendText(string.Format("DoSomethingLong方法开始，按钮名称：{0} 线程ID： {1} 开始时间： {2} \n", name, Thread.CurrentThread.ManagedThreadId.ToString("00"), DateTime.Now.ToString("HHmmss:fff")));
+
+            var result = 0;
+            for (int i = 0; i < 100000; i++)
+            {
+                result += i;
+            }
+            rtb.AppendText(string.Format("DoSomethingLong方法结束，按钮名称：{0} 线程ID： {1} 开始时间： {2} \n", name, Thread.CurrentThread.ManagedThreadId.ToString("00"), DateTime.Now.ToString("HHmmss:fff")));
+        }
+    }
+}
