dbbfcea511718cc03b6bb65d2ebdbe91bd403b51
强 梁 <673615750@qq.com>
2021-01-17 13:58:51 +0800

    添加lock测试用例。

--- StudyThread/MutipleCompare.cs
+++ StudyThread/MutipleCompare.cs
@@ -115,8 +115,120 @@ namespace StudyThread
             rtb_CompareText.AppendText(string.Format("btn_ThreadSafety_Click异步方法开始 ID：{0} \n", Thread.CurrentThread.ManagedThreadId));
 
 
+            #region 线程安全问题 单线程与多线程
+            //多线程安全问题：一段代码，由单线程跟多线程执行，结果不一致，就表明有线程安全问题。
+            //List集合是个线性结构，在内存上是连续存储的，
+            //添加一个数量就操作一个内存位置，如果多个cpu同时发出命令，
+            //内存先执行一个再执行另一个，会出现覆盖，导致内存地址值发生改变。
+            var iList = new List<int>();
+            for (int i = 0; i < 50; i++)
+            {
+                //单线程
+                iList.Add(i);
+            }
+            rtb_CompareText.AppendText(string.Format("List集合单线程数量：{0} ID：{1} \n", iList.Count, Thread.CurrentThread.ManagedThreadId));
+
+            iList = new List<int>();
+            for (int i = 0; i < 50; i++)
+            {
+                //多线程之后，结果数据会少于预计的数量，有些数据会丢失。
+                Task.Run(() =>
+                {
+                    iList.Add(i);
+                });
+            }
+            Thread.Sleep(200);
+            rtb_CompareText.AppendText(string.Format("List集合多线程数量：{0} ID：{1} \n", iList.Count, Thread.CurrentThread.ManagedThreadId));
+
+            #endregion
+
+            #region 解决线程安全问题
+            iList = new List<int>();
+            for (int i = 0; i < 50; i++)
+            {
+                //多线程之后，结果数据会少于预计的数量，有些数据会丢失。
+                Task.Run(() =>
+                {
+                    //加lock能保证任意时刻只有一个线程进去，其他线程需要排队等待（单线程化）。
+                    //lock原理=>Monitor----锁定一个内存引用地址，所以不能是值类型或null。
+                    lock (obj)
+                    {
+                        iList.Add(i);
+                    }
+
+                });
+            }
+            Thread.Sleep(200);
+            rtb_CompareText.AppendText(string.Format("List集合加锁多线程数量：{0} ID：{1} \n", iList.Count, Thread.CurrentThread.ManagedThreadId));
+            #endregion
+
+            #region lock测试
+
+            lockTest.ShowTest(rtb_CompareText);
+            for (int i = 0; i < 5; i++)
+            {
+                int k = i;
+                Task.Run(() =>
+                {
+                    //加锁后 一个线程执行完然后才允许其他线程执行。
+                    lock (obj)//共用同一个锁变量，会出现阻塞，如果需要并发，要定义不同的锁变量。
+                    {
+                        rtb_CompareText.AppendText(string.Format("ShowTest主线程当前i={0} k={1} ID：{2} 开始 \n", i, k, Thread.CurrentThread.ManagedThreadId));
+                        Thread.Sleep(2000);
+                        rtb_CompareText.AppendText(string.Format("ShowTest主线程当前i={0} k={1} ID：{2} 结束\n", i, k, Thread.CurrentThread.ManagedThreadId));
+                    }
+                });
+            }
+
+
+            //实例化对象后调用能实现并发
+            //是因为lock里面的引用地址经过实例化后会重新生成新的引用地址。
+            //如果是static就不行，因为static是系统全局变量，
+            //每次实例化后静态变量是不会重新生成的，除非重新启动程序。
+            lockTest testLock = new lockTest();
+            testLock.ShowTemp(rtb_CompareText, 1);
+
+            lockTest testLock2 = new lockTest();
+            testLock2.ShowTemp(rtb_CompareText, 2);
+
+
+            lockTest testLock3 = new lockTest();
+            testLock3.ShowString(rtb_CompareText, 1);
+
+            lockTest testLock4 = new lockTest();
+            testLock4.ShowString(rtb_CompareText, 2);
+
+            for (int i = 0; i < 5; i++)
+            {
+                int k = i;
+                Task.Run(() =>
+                {
+                    //Lock锁定的是引用地址，因为LockObjString的引用地址一致，所以不能并发。
+                    lock (LockObjString)
+                    {
+                        rtb_CompareText.AppendText(string.Format("ShowString主线程当前i={0} k={1} ID：{2} 开始 \n", i, k, Thread.CurrentThread.ManagedThreadId));
+                        Thread.Sleep(2000);
+                        rtb_CompareText.AppendText(string.Format("ShowString主线程当前i={0} k={1} ID：{2} 结束\n", i, k, Thread.CurrentThread.ManagedThreadId));
+                    }
+                });
+            }
+
+
+            //泛型lock
+            //两个int不能并发，因为泛型类在相同参数下，是同一个类。
+            //int与string参数不同，所以生成的引用地址也不同。
+            lockTestGeneric<int>.ShowGeneric(rtb_CompareText, 1);
+            lockTestGeneric<int>.ShowGeneric(rtb_CompareText, 1);
+            lockTestGeneric<string>.ShowGeneric(rtb_CompareText, 1);
+
+            new lockTest().ShowThisAnother(rtb_CompareText, 1);
+            #endregion
 
             rtb_CompareText.AppendText(string.Format("btn_ThreadSafety_Click异步方法结束 ID：{0} \n", Thread.CurrentThread.ManagedThreadId));
         }
+
+        private static readonly object obj = new object();
+
+        private readonly string LockObjString = "测试lock字符串";
     }
 }
---
+++ StudyThread/lockTest.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StudyThread
+{
+    public class lockTest
+    {
+
+        private static readonly object LockObj = new object();
+
+        private readonly object LockObjTemp = new object();
+
+        private readonly string LockObjString = "测试lock字符串";
+        public static void ShowTest(System.Windows.Forms.RichTextBox rtb_CompareText)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                int k = i;
+                Task.Run(() =>
+                {
+                    lock (LockObj)
+                    {
+                        rtb_CompareText.AppendText(string.Format("lockTest.ShowTest当前i={0} k={1} ID：{2} 开始 \n", i, k, Thread.CurrentThread.ManagedThreadId));
+                        Thread.Sleep(2000);
+                        rtb_CompareText.AppendText(string.Format("lockTest.ShowTest当前i={0} k={1} ID：{2} 结束\n", i, k, Thread.CurrentThread.ManagedThreadId));
+                    }
+                });
+            }
+
+        }
+
+
+        public void ShowTemp(System.Windows.Forms.RichTextBox rtb_CompareText, int index)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                int k = i;
+                Task.Run(() =>
+                {
+                    lock (LockObjTemp)
+                    {
+                        rtb_CompareText.AppendText(string.Format("lockTest.ShowTemp{3}当前i={0} k={1} ID：{2} 开始 \n", i, k, Thread.CurrentThread.ManagedThreadId, index));
+                        Thread.Sleep(2000);
+                        rtb_CompareText.AppendText(string.Format("lockTest.ShowTemp{3}当前i={0} k={1} ID：{2} 结束\n", i, k, Thread.CurrentThread.ManagedThreadId, index));
+                    }
+                });
+            }
+
+        }
+
+
+        public void ShowString(System.Windows.Forms.RichTextBox rtb_CompareText, int index)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                int k = i;
+                Task.Run(() =>
+                {
+                    lock (LockObjString)
+                    {
+                        rtb_CompareText.AppendText(string.Format("lockTest.ShowString{3}当前i={0} k={1} ID：{2} 开始 \n", i, k, Thread.CurrentThread.ManagedThreadId, index));
+                        Thread.Sleep(2000);
+                        rtb_CompareText.AppendText(string.Format("lockTest.ShowString{3}当前i={0} k={1} ID：{2} 结束\n", i, k, Thread.CurrentThread.ManagedThreadId, index));
+                    }
+                });
+            }
+
+        }
+
+        private int _num = 0;
+        public void ShowThisAnother(System.Windows.Forms.RichTextBox rtb_CompareText, int index)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                _num++;
+                int k = i;
+                lock (this)
+                {
+                    rtb_CompareText.AppendText(string.Format("lockTest.ShowThisAnother{3}当前i={0} k={1} ID：{2} 开始 \n", i, k, Thread.CurrentThread.ManagedThreadId, index));
+                    Thread.Sleep(2000);
+                    rtb_CompareText.AppendText(string.Format("lockTest.ShowThisAnother{3}当前i={0} k={1} ID：{2} 结束\n", i, k, Thread.CurrentThread.ManagedThreadId, index));
+                    if (this._num < 5)
+                    {
+                        this.ShowThisAnother(rtb_CompareText, index);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+
+    public class lockTestGeneric<T>
+    {
+
+        private static readonly object LockObj = new object();
+        public static void ShowGeneric(System.Windows.Forms.RichTextBox rtb_CompareText, int index)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                int k = i;
+                Task.Run(() =>
+                {
+                    lock (LockObj)
+                    {
+                        rtb_CompareText.AppendText(string.Format("lockTestGeneric.ShowGeneric{3}当前i={0} k={1} ID：{2} 开始 \n", i, k, Thread.CurrentThread.ManagedThreadId, index));
+                        Thread.Sleep(2000);
+                        rtb_CompareText.AppendText(string.Format("lockTestGeneric.ShowGeneric{3}当前i={0} k={1} ID：{2} 结束\n", i, k, Thread.CurrentThread.ManagedThreadId, index));
+                    }
+                });
+            }
+
+
+        }
+    }
+}
